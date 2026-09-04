3a9988a0f3b2d50580b6e9703b454268904cc140
Vitor Lima <viitorlima2000@gmail.com>
2026-09-04 06:15:38 -0300

    Configurar Sass

--- quasar.conf.js
+++ quasar.conf.js
@@ -28,7 +28,7 @@ module.exports = function (/* ctx */) {
 
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.css'
+      'app.sass'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
