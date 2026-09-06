2d326da9f32920d712a77db583bed0cb04eccd0f
Vitor Lima <viitorlima2000@gmail.com>
2026-09-06 15:54:03 -0300

    Bindar "middlewares.userGetUserByUidRequest" em "user.routes.ts"

--- backend/src/infra/http/routes/v1/user.routes.ts
+++ backend/src/infra/http/routes/v1/user.routes.ts
@@ -1,11 +1,15 @@
 import { type Request, type Response, Router } from "express";
 
+import middlewares from "../../middlewares/index.ts";
 import UserController from "../../controllers/v1/UserController.ts";
 
 const router = Router();
 
-router.get("/:uid", (req: Request, res: Response) =>
-    new UserController().getUserByUid(req, res),
+router.get(
+    "/:uid",
+    middlewares.userGetUserByUidRequest,
+    (req: Request, res: Response) =>
+        new UserController().getUserByUid(req, res),
 );
 
 export default router;
