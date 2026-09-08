212f8cb2d34d7e4bee79bf13481271068fb074dd
Vitor Lima <viitorlima2000@gmail.com>
2026-09-08 11:27:24 -0300

    Adicionar uma factory do middleware do multer

--- backend/src/infra/http/middlewares/MulterMiddleware.ts
+++ backend/src/infra/http/middlewares/MulterMiddleware.ts
@@ -1,77 +1,136 @@
+import type { Request } from 'express';
 import multer from 'multer';
 
 import AppError from '../../../app/errors/AppError.ts';
-import verifyIfArrayLengthGtZero from '../../../app/utils/verifyIfArrayLengthGtZero.ts';
 
-/**
- * @refactor
- * 
- * input: {
- *  formFileFields: {
- *    acceptedMimetypes: string[];
- *    fieldName: string;
- *    maxNumberOfFiles: number;
- *   }[],
- *   maximumFileSize?: number
- * }
- */
+const DEFAULT_FILE_SIZE = 10_000_000; // 10 MB in bytes
+
 export default (input: {
-  acceptedMimetypes: string[];
-  formFieldNames: string[];
-  maxNumberOfFiles: number;
-  fileSize?: number;
+  req: Request;
+  formFileFields: {
+    acceptedMimetypes: string[];
+    fieldName: string;
+    maxNumberOfFiles: number;
+  }[];
+  maxFileSize?: number;
 }) => {
-  if (
-    !verifyIfArrayLengthGtZero(input.acceptedMimetypes)
-    || !verifyIfArrayLengthGtZero(input.formFieldNames)
-  ) {
+  if (!input.req.file || !input.req.files) {
     throw new AppError({
-      message: 'acceptedMimetypes or formFieldNames must be gt than 0',
-      errorCode: 'ERROR_ON_MulterMiddleware',
+      message: 'req.file or req.files is undefined',
+      errorCode: 'req.file"s"_IS_UNDEFINED',
     });
   }
 
-  if (!input.maxNumberOfFiles) {
+  if (!input.formFileFields.length) {
     throw new AppError({
-      message: 'maxNumberOfFiles must be gt than 0',
-      errorCode: 'ERROR_ON_MulterMiddleware',
+      message: 'formFileFields lenght must be gt 0',
+      errorCode: 'INVALID_ARRAY_LENGTH',
     });
   }
 
-  const defaultFileSize = 10_000_000 // 10 MB
-  const storage = multer.memoryStorage();
+  const commonConfigForMiddleware: multer.Options = {
+    limits: {
+      files: input.formFileFields.length,
+      fileSize: input.maxFileSize ? input.maxFileSize : DEFAULT_FILE_SIZE,
+    },
+  };
 
-  if (input.maxNumberOfFiles === 1) {
+  if (input.formFileFields.length === 1) {
+    const fieldWithFileOrFilesToUpload = input.formFileFields[0]!; // I already verify that input.formFileFields[0] is truthy on if (!input.formFileFields.length)
+
+    if (!fieldWithFileOrFilesToUpload.acceptedMimetypes.length) {
+      throw new AppError({
+        message: 'acceptedMimetypes lenght must be gt 0',
+        errorCode: 'INVALID_ARRAY_LENGTH',
+      });
+    }
+    if (!fieldWithFileOrFilesToUpload.maxNumberOfFiles) {
+      throw new AppError({
+        message: 'maxNumberOfFiles must be gt 0',
+        errorCode: 'INVALID_NUMBER',
+      });
+    }
+
+    const commonFileFilter: multer.Options['fileFilter'] = (_, file, cb) => {
+      if (
+        !fieldWithFileOrFilesToUpload.acceptedMimetypes.includes(file.mimetype)
+      ) {
+        cb(null, false);
+
+        throw new AppError({
+          message: 'req.file.mimetype is not accepted',
+          errorCode: 'INVALID_FILE_MIMETYPE',
+        });
+      }
+
+      cb(null, true);
+    };
+
+    if (fieldWithFileOrFilesToUpload.maxNumberOfFiles === 1) {
+      return multer({
+        ...commonConfigForMiddleware,
+        fileFilter: commonFileFilter,
+      }).single(fieldWithFileOrFilesToUpload.fieldName);
+    }
+
     return multer({
-      limits: { fileSize: input.fileSize },
-      fileFilter: (req, _, cb) => {
-        if (req.file && !input.acceptedMimetypes.includes(req.file.mimetype)) {
-          throw new AppError({
-            message:
-              'input.acceptedMimetypes does not includes req.file.mimetype',
-            errorCode: 'ERROR_ON_MulterMiddleware',
-          });
-        }
-
-        cb(null, true);
-      },
-      storage,
-    }).single(input.formFieldNames[0]!); // Already verified that typeof input.formFieldNames[0] === 'string' on !verifyIfArrayLengthGtZero(input.formFieldNames)
-  } else {
-    return multer({
-      limits: { fileSize: input.fileSize, files: input.maxNumberOfFiles },
-      fileFilter: (req, _, cb) => {
-        if (req.file && !input.acceptedMimetypes.includes(req.file.mimetype)) {
-          throw new AppError({
-            message:
-              'input.acceptedMimetypes does not includes req.file.mimetype',
-            errorCode: 'ERROR_ON_MulterMiddleware',
-          });
-        }
-
-        cb(null, true);
-      },
-      storage,
-    }).fields(input.formFieldNames[0]!);
+      ...commonConfigForMiddleware,
+      fileFilter: commonFileFilter,
+    }).array(
+      fieldWithFileOrFilesToUpload.fieldName,
+      fieldWithFileOrFilesToUpload.maxNumberOfFiles,
+    );
   }
+
+  return multer({
+    ...commonConfigForMiddleware,
+    fileFilter: (req, _, cb) => {
+      const filesFromReqFiles = req.files!; // I already verify that req.files is truthy on if (!req.file || !req.files)
+
+      if (Array.isArray(filesFromReqFiles)) {
+        cb(null, false);
+
+        throw new AppError({
+          message: 'req.files is an array instead of a object',
+          errorCode: 'req.files_IS_AN_ARRAY',
+        });
+      }
+
+      for (const formFileField of input.formFileFields) {
+        const filesFromFormFileField =
+          filesFromReqFiles[formFileField.fieldName];
+
+        if (!filesFromFormFileField) {
+          cb(null, false);
+
+          throw new AppError({
+            message: 'filesFromFormFileField is undefined',
+            errorCode: 'filesFromFormFileField_IS_UNDEFINED',
+          });
+        }
+
+        for (const fileFromFormFileField of filesFromFormFileField) {
+          if (
+            !formFileField.acceptedMimetypes.includes(
+              fileFromFormFileField.mimetype,
+            )
+          ) {
+            cb(null, false);
+
+            throw new AppError({
+              message: 'req.file.mimetype is not accepted',
+              errorCode: 'INVALID_FILE_MIMETYPE',
+            });
+          }
+
+          cb(null, true);
+        }
+      }
+    },
+  }).fields(
+    input.formFileFields.map((formFileField) => ({
+      name: formFileField.fieldName,
+      maxCount: formFileField.maxNumberOfFiles,
+    })),
+  );
 };
